8466983de03ab66cd6b94ead74736471ad3a820f
Harman Singh Hira <HSinghHira@users.noreply.github.com>
2021-09-23 21:10:24 +1200

    Update main.js

--- main.js
+++ main.js
@@ -3888,3 +3888,23 @@ var _self = "undefined" != typeof window ? window : "undefined" != typeof Worker
     }
 }));
 //# sourceMappingURL=bootstrap.bundle.min.js.map
+function saveit(e, t) {
+    var n = new Blob([e], {
+            type: "text/plain"
+        }),
+        o = document.createElement("a");
+    o.download = t, o.innerHTML = "Download File", null != window.webkitURL ? o.href = window.webkitURL.createObjectURL(n) : (o.href = window.URL.createObjectURL(n), o.onclick = destroyClickedElement, o.style.display = "none", document.body.appendChild(o)), o.click()
+}
+
+function loadit() {
+    var e = document.getElementById("fileToLoad").files[0],
+        t = new FileReader;
+    t.onload = function(e) {
+        var t = e.target.result;
+        document.getElementById("maindata").value = t
+    }, t.readAsText(e, "UTF-8")
+}
+
+function copyit() {
+    document.getElementById("maindata").select(), document.execCommand("copy")
+}
